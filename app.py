04d19eaad076af384a6e134efa78fa85dab5138c
Aneesh Kodali <aneesh.kodali@gmail.com>
2020-12-06 17:21:27 -0500

    Fixed not importing db when running the app

--- app.py
+++ app.py
@@ -15,13 +15,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'jose'
 api = Api(app)
 
-# create db if DNE before first request is run
-# SQLAlchemy can only create tables it sees (it sees tables because we import the resource which imports the model)
-# we can also import models directly and db will create
-# ex. import Store resource <= Store imports StoreModel <= StoreModel has tablename
-@app.before_first_request
-def create_tables():
-    db.create_all()
 
 # use jwt in app to authenticate users
 jwt = JWT(app, authenticate, identity)
